38bb0fbea8572daae608dcdc76f8031d61bac5cc
christenally <13616443+Christenally@users.noreply.github.com>
2026-03-29 00:09:50 -0500

    Working on debouncing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,46 @@
+import { useEffect, useState } from "react";
+
 import Navbar from "./Navbar";
 import ThemeObserver from "./ThemeProvider";
 import MainContainer from "./MainContainer";
 import Search from "./Search";
 import Filter from "./Filter";
 
+import useFetch from "./Fetching";
+
 function App() {
+  //query comes from search
+  const [query, setQuery] = useState(null);
+  const [timedQuery, setTimedQuery] = useState(null);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimedQuery();
+    }, 500);
+  }, [query]);
+
+  function userInput(input) {
+    setQuery(input);
+  }
+
+  /*
+  this will update when query updates
+  ? setQuery causes re-render 
+  * useFetch template literal updates 
+  * isData is updated ---> we need to place isData somewhere
+
+   */
+  const { isData } = useFetch(
+    ` https://restcountries.com/v3.1/name/${timedQuery}?fullText=true`,
+  );
+
   return (
     <div className="h-full">
       <ThemeObserver>
         <Navbar></Navbar>
         <MainContainer>
           <div className="w-full flex flex-col gap-8 ">
-            <Search></Search>
+            <Search userInput={userInput}></Search>
             <Filter></Filter>
           </div>
         </MainContainer>
--- src/Search.jsx
+++ src/Search.jsx
@@ -1,7 +1,7 @@
 import { useTheme } from "./ThemeProvider";
 import { SearchIcon } from "lucide-react";
 
-export default function Search() {
+export default function Search({ userInput }) {
   const { theme } = useTheme();
   return (
     <div
@@ -9,6 +9,9 @@ export default function Search() {
     >
       <SearchIcon size={15} color={theme ? "#808080" : "white"}></SearchIcon>
       <input
+        //this needs to be changed
+        //debouncing issue
+        onChange={(e) => userInput(e.target.value)}
         className={`w-full h-12 text-left pl-5 focus:outline-0 text-preset-6r ${theme ? " " : " text-white"} `}
         type="search"
         placeholder="Search for a country..."
